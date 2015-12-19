a3f840652ac874a65248f981c7b61db0f7e85261
Count_dev <tyler6389@users.noreply.github.com>
2015-12-19 22:47:59 +0900

    Modify typing error

--- build_lollipop.sh
+++ build_lollipop.sh
@@ -3,6 +3,6 @@
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 make countdev_defconfig
-make -16
+make -j16
 echo Android Lollipop Kernel Build Completed...
 echo Enjoy~!
